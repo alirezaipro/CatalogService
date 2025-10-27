a85b6a7893bd48190274b53067ac7b2938b92ebe
ali rezaei <alirezai.pro@gmail.com>
2025-10-27 10:17:02 +0330

    feat: add catalog brand endpoints

--- src/Catalog/Endpoints/CatalogBrandEndpoints.cs
+++ src/Catalog/Endpoints/CatalogBrandEndpoints.cs
@@ -14,11 +14,14 @@ public static class CatalogBrandEndpoints
     {
         app.MapPost("/", CreateBrand);
         app.MapPut("/", UpdateBrand);
-
+        app.MapGet("/", GetBrands);
+        app.MapGet("/{id:required:int}", GetBrandById);
+        app.MapDelete("/{id:required:int}", DeleteBrandById);
+        
         return app;
     }
 
-    public static async Task<Results<Created, ValidationProblem, BadRequest<string>>> CreateBrand(
+    private static async Task<Results<Created, ValidationProblem, BadRequest<string>>> CreateBrand(
         CreateCatalogBrandRequest model,
         IValidator<CreateCatalogBrandRequest> validator,
         [AsParameters] CatalogServices services,
@@ -31,9 +34,7 @@ public static class CatalogBrandEndpoints
 
         var hasBrand = await services.Context.CatalogBrands.AnyAsync(x => x.Brand == model.Brand, cancellationToken);
         if (hasBrand)
-        {
             return TypedResults.BadRequest($"A brand with the name '{model.Brand}' already exists.");
-        }
 
         var brand = CatalogBrand.Create(model.Brand);
 
@@ -43,7 +44,7 @@ public static class CatalogBrandEndpoints
         return TypedResults.Created($"/catalog/api/v1/brands/{brand.Id}");
     }
 
-    public static async Task<Results<Created, ValidationProblem, NotFound<string>>> UpdateBrand(
+    private static async Task<Results<Created, ValidationProblem, NotFound<string>>> UpdateBrand(
         UpdateCatalogBrandRequest model,
         [AsParameters] CatalogServices services,
         IValidator<UpdateCatalogBrandRequest> validator,
@@ -53,16 +54,58 @@ public static class CatalogBrandEndpoints
 
         if (!validate.IsValid)
             return TypedResults.ValidationProblem(validate.ToDictionary());
-        
+
         var brand = await services.Context.CatalogBrands.FirstOrDefaultAsync(i => i.Id == model.Id, cancellationToken);
         if (brand is null)
-        {
             return TypedResults.NotFound($"Brand with id {model.Id} not found.");
-        }
 
         brand.Update(model.Brand);
         await services.Context.SaveChangesAsync(cancellationToken);
 
         return TypedResults.Created($"/catalog/api/v1/brands/{brand.Id}");
+    }
+
+    private static async Task<Results<Ok<CatalogBrandResponse>, BadRequest<string>, NotFound<string>>> GetBrandById(
+        [AsParameters] CatalogServices services,
+        int id)
+    {
+        if (id <= 0)
+            return TypedResults.BadRequest("Id is not valid.");
+
+        var brand = await services.Context.CatalogBrands.FirstOrDefaultAsync(ci => ci.Id == id);
+        if (brand is null)
+            return TypedResults.NotFound("Catalog not found");
+
+
+        return TypedResults.Ok(new CatalogBrandResponse(id, brand.Brand));
+    }
+    
+    public static async Task<Results<Ok<IEnumerable<CatalogBrandResponse>>, BadRequest<string>>> GetBrands(
+        [AsParameters] CatalogServices services,
+        CancellationToken cancellationToken)
+    {
+        var brands = await services.Context.CatalogBrands
+            .OrderBy(c => c.Id)
+            .Select(x => new CatalogBrandResponse(x.Id, x.Brand))
+            .ToListAsync(cancellationToken);
+
+        return TypedResults.Ok<IEnumerable<CatalogBrandResponse>>(brands);
+    }
+    
+    
+    public static async Task<Results<NoContent, NotFound, BadRequest<string>>> DeleteBrandById
+        ([AsParameters] CatalogServices services, int id, CancellationToken cancellationToken)
+    {
+        if (id <= 0)
+            return TypedResults.BadRequest("Id is not valid.");
+
+        var brand = await services.Context.CatalogBrands.FirstOrDefaultAsync(x => x.Id == id, cancellationToken: cancellationToken);
+        if (brand is null)
+            return TypedResults.NotFound();
+
+        services.Context.CatalogBrands.Remove(brand);
+        await services.Context.SaveChangesAsync(cancellationToken);
+        
+        return TypedResults.NoContent();
     }
 }
---
+++ src/Catalog/Endpoints/Contracts/CatalogBrand/CatalogBrandResponse.cs
@@ -0,0 +1,3 @@
+namespace Catalog.Endpoints.Contracts.CatalogBrand;
+
+public sealed record CatalogBrandResponse(int Id, string Brand);
